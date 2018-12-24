d459181935d0d88f74c849c7343c60723994ea70
xxxFD <mbv40bp205@gmail.com>
2018-12-24 19:33:51 +0800

    v0.1

--- class_SIS.py
+++ class_SIS.py
@@ -6,18 +6,18 @@ import numpy as np
 # ################
 # init
 # ################
-initial = 0.001
-run_times = 10
-step = 100
-gama = 0.08
-beta = 0.15
+initial = 0.001                 # the initial rate of I
+run_times = 10                  # the times of run
+step = 100                      # the number of step ever times
+gama = 0.08                     # the rate of I to S
+beta = 0.15                     # the rate of S to I
 
-result = dict()
-result[0] = initial
+result = dict()                 # the ratio of each step I to the total
+result[0] = initial             # the ratio of I to the total at the begin
 for i in range(1, step):
     result[i] = []
 
-fp = open('e:/paper_data/hash_without_time/Foxconn worker falls to death')
+fp = open('scale_free_network')     # the topology of scale-free network
 edge = []
 for line in fp:
     (a, b) = line.split()
@@ -25,17 +25,17 @@ for line in fp:
 fp.close()
 g = nx.Graph()
 g.add_edges_from(edge)
-node_num = g.number_of_nodes()
+node_num = g.number_of_nodes()      # the number of node
 
 # #####################################
 # main
 # ####################################
 
 for times in range(run_times):
-    I_set = []
-    state = [0] * (node_num + 1)
+    I_set = []                                      # the set of I
+    state = [0] * (node_num + 1)                    # the sign of state, 0 means that S, 1 means that I
 
-    for i in range(int(initial * node_num)):
+    for i in range(int(initial * node_num)):        # random select some I at begin
         temp = random.randint(1, node_num)
         while state[temp] == 1:
             temp = random.randint(1, node_num)
@@ -43,8 +43,8 @@ for times in range(run_times):
         I_set.append(temp)
 
     for i in range(1, step):
-        I_new = []
-        S_new = []
+        I_new = []                                  # the new I is generate at each step
+        S_new = []                                  # the recovery of node at each step
         for j in I_set:
             for k in nx.neighbors(g, j):
                 if state[k] == 0:
